18df96ec6d214ed1ec371989a9742c4daafea947
Myles Carter <112006452+Myles-Carter@users.noreply.github.com>
2024-02-22 17:19:55 -0500

    Created CourseList Class stub #21

---
+++ CourseList.java
@@ -0,0 +1,17 @@
+package advising;
+
+public class CourseList {
+    private CouseList courseList;
+
+    private CourseList() {
+
+    }
+
+    public CourseList getInstance() {
+
+    }
+
+    public void findCourse(String course) {
+    
+    }
+}
