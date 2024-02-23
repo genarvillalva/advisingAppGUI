6df9de4cf5108ac4e297c0cc22efccd19c3bae08
Genar Villalva <genarvillalva@Genars-MacBook-Air.local>
2024-02-22 21:28:38 -0500

    #21 created student class stub

--- advising/Student.java
+++ advising/Student.java
@@ -2,4 +2,17 @@ package advising;
 
 public class Student {
 
+  private String major;
+  private Advisor advisor;
+  private StudentYear studentClass;
+  private StudentPortfolio studentPortfolio;
+  private String applicationArea;
+
+  public Student() {}
+
+  public void addCoursesToStudentPortfolio(String major) {}
+
+  public void displayMajorMap(Major major) {}
+
+  public void displayStudentPortfolio() {}
 }
