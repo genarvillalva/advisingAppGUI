c3ca7ac0eef94083760d243b6586439d6de0d6b1
Genar Villalva <genarvillalva@Genars-MacBook-Air.local>
2024-02-24 08:44:17 -0500

    fixed #7

--- advising/Student.java
+++ advising/Student.java
@@ -8,7 +8,13 @@ public class Student {
   private StudentPortfolio studentPortfolio;
   private String applicationArea;
 
-  public Student() {}
+  public Student(String major, Advisor advisor, StudentYear studentClass, StudentPortfolio portfolio, String applicationArea) {
+    this.major = major;
+    this.advisor = advisor;
+    this.studentClass = studentClass;
+    this.studentPortfolio = portfolio;
+    this.applicationArea = applicationArea;
+  }
 
   public void addCoursesToStudentPortfolio(String major) {}
 
