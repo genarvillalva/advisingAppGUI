d9700cb4532fb23c806706cc1584bcbad31ca7fb
timmy <timey47@gmail.com>
2024-02-24 11:56:45 -0500

    Admin Class stub

---
+++ advising/Admin.java
@@ -0,0 +1,21 @@
+package advising;
+
+import java.util.ArrayList;
+
+public class Admin {
+    private ArrayList<Student> listOfAllStudents;
+
+
+    // Method to create a course
+    public void createCourse(Course course) {
+        
+    }
+
+    // Method to delete a course
+    public void deleteCourse(Course course) {
+       
+    }
+    
+
+}
+
