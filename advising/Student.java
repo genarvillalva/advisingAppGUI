72a6d934b3ebd4af040806c8be4f2b62440ea57d
Genar Villalva <genarvillalva@Genars-MacBook-Air.local>
2024-02-24 15:30:16 -0500

    updated #7

--- advising/Student.java
+++ advising/Student.java
@@ -1,17 +1,24 @@
 package advising;
 
 public class Student {
-
+  private String firstName;
+  private String lastName;
+  private String username;
+  private String password;
   private String major;
   private Advisor advisor;
   private StudentYear studentClass;
   private StudentPortfolio studentPortfolio;
   private String applicationArea;
 
-  public Student(String major, Advisor advisor, StudentYear studentClass, StudentPortfolio portfolio, String applicationArea) {
+  public Student(String firstName, String lastName, String username, String password, String major, Advisor advisor, StudentYear studentYear, StudentPortfolio portfolio, String applicationArea) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
     this.major = major;
     this.advisor = advisor;
-    this.studentClass = studentClass;
+    this.studentClass = studentYear;
     this.studentPortfolio = portfolio;
     this.applicationArea = applicationArea;
   }
