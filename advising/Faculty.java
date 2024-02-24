102018e63ae5b9fabceb597a1a57dce7f1ff86fb
timmy <timey47@gmail.com>
2024-02-24 11:35:15 -0500

    added Faculty class stub

---
+++ advising/Faculty.java
@@ -0,0 +1,24 @@
+package advising;
+
+    public class Faculty {
+  
+  
+    // Returns a String, which could be details of the student profile.
+    public String searchStudentProfile(String username) {
+       
+        return "Student Profile Details for " + username;
+    }
+    
+    // Method to access a student's profile using their username.
+    public void accessStudentProfile(String username) {
+       
+    }
+    
+    // Method to input or update the grades of a student.
+    public void inputStudentGrades(int grade) {
+        
+    }
+    
+
+}
+
