c6a29b27ee73ea6aace768d96bc67899f6f5d080
genarvillalva <genar@email.sc.edu>
2024-02-24 16:53:23 -0500

    Added advisor getters #10

--- advising/Student.java
+++ advising/Student.java
@@ -1,6 +1,7 @@
 package advising;
 
 public class Student {
+
   private String firstName;
   private String lastName;
   private String username;
@@ -10,19 +11,30 @@ public class Student {
   private StudentYear studentClass;
   private StudentPortfolio studentPortfolio;
   private String applicationArea;
-/**
- * Constructor for the Student class
- * @param firstName
- * @param lastName
- * @param username
- * @param password
- * @param major
- * @param advisor
- * @param studentYear
- * @param portfolio
- * @param applicationArea
- */
-  public Student(String firstName, String lastName, String username, String password, String major, Advisor advisor, StudentYear studentYear, StudentPortfolio portfolio, String applicationArea) {
+
+  /**
+   * Constructor for the Student class
+   * @param firstName
+   * @param lastName
+   * @param username
+   * @param password
+   * @param major
+   * @param advisor
+   * @param studentYear
+   * @param portfolio
+   * @param applicationArea
+   */
+  public Student(
+    String firstName,
+    String lastName,
+    String username,
+    String password,
+    String major,
+    Advisor advisor,
+    StudentYear studentYear,
+    StudentPortfolio portfolio,
+    String applicationArea
+  ) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.username = username;
@@ -33,6 +45,7 @@ public class Student {
     this.studentPortfolio = portfolio;
     this.applicationArea = applicationArea;
   }
+
   /**
    * Get the first name of the student
    * @return The first name of the student
@@ -40,6 +53,7 @@ public class Student {
   public String getFirstName() {
     return firstName;
   }
+
   /**
    * Get the last name of the student
    * @return The last name of the student
@@ -47,6 +61,7 @@ public class Student {
   public String getLastName() {
     return lastName;
   }
+
   /**
    * Get the username of the student
    * @return The username of the student
@@ -54,6 +69,7 @@ public class Student {
   public String getUserName() {
     return username;
   }
+
   /**
    * Get the password of the student
    * @return The password of the student
@@ -61,6 +77,7 @@ public class Student {
   public String getPassword() {
     return password;
   }
+
   /**
    * Get the major of the student
    * @return The major of the student
@@ -68,6 +85,7 @@ public class Student {
   public String getMajor() {
     return major;
   }
+
   /**
    * Get the advisor of the student
    * @return The advisor of the student
@@ -75,6 +93,7 @@ public class Student {
   public Advisor getAdvisor() {
     return advisor;
   }
+
   /**
    * Get the student year of the student
    * @return The student year of the student
@@ -82,6 +101,7 @@ public class Student {
   public StudentYear getStudentClass() {
     return studentClass;
   }
+
   /**
    * Get the student portfolio of the student
    * @return The student portfolio of the student
@@ -89,6 +109,7 @@ public class Student {
   public StudentPortfolio getPortfolio() {
     return studentPortfolio;
   }
+
   /**
    * Get the application area of the student
    * @return The application area of the student
