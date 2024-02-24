81582748babeb073b2394b33c03b9f4ebd4e26b3
R0bb1eV <josephvann536@gmail.com>
2024-02-24 08:19:16 -0500

    #140 added to ElectiveCluster Class

--- advising/ElectiveCluster.java
+++ advising/ElectiveCluster.java
@@ -1,5 +1,21 @@
 package advising;
 
+import java.util.HashMap;
+
 public class ElectiveCluster {
+  private HashMap<Course, Boolean> Courses;
+
+  // are these methods used to be private?
+  private boolean isClusterComplete() {
+
+    return true;
+  }
+
+  private int hoursCompleted() {
+
+    return 0;
+  }
+
+  private int hoursRequired;
 
 }
