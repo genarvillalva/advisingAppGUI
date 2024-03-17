c15968236ecc47ee0a7872fb69f9e70954f95693
Myles Carter <112006452+Myles-Carter@users.noreply.github.com>
2024-03-17 17:25:57 -0400

    Print outs

--- advising/StudPortTestFile.java
+++ advising/StudPortTestFile.java
@@ -1,11 +1,52 @@
 package advising;
 
+import java.util.ArrayList;
+
 public class StudPortTestFile {
     public static void main(String[] args) {
         
-        StudentPortfolio.printAllStudentPortfolios();
-        //String filePath = "student_portfolios.txt";
-        //StudentPortfolio.printAllStudentPortfoliosToFile(filePath);
+/*
+        UserList userList = UserList.getInstance();
+        ArrayList<Student> students = userList.getStudents();
+       // System.out.println(students.get(0).getPortfolio().getEightSemesterPlan());
+
+        
+        System.out.println("Portfolio UUID: " + students.get(0).getPortfolio().getPortfolioUUID());
+            //System.out.println("Required Courses: " + portfolio.getRequiredCourses());
+            System.out.println("\nEight Semester Plan:");
+            students.get(0).getPortfolio().getEightSemesterPlan().forEach((semester, courses) -> {
+                System.out.print("\nSemester " + semester + ": \n");
+                courses.forEach(course -> System.out.print(course +"\n"));
+            });
+
+*/
+
+
+      
+/* 
+        public static void printEightSemesterPlan() {
+            StudentPortfolio studentPortfolio = StudentPortfolio.printAllStudentPortfolios();
+            System.out.println("Portfolio UUID: " + studentPortfolio.getPortfolioUUID());
+            System.out.println("\nEight Semester Plan:");
+            studentPortfolio.getEightSemesterPlan().forEach((semester, courses) -> {
+              System.out.print("\nSemester " + semester + ": ");
+              courses.forEach(course -> System.out.print(course + ", "));
+              System.out.println();
+            });
+          }
+
+        for(StudentPortfolio portfolio : StudentPortfolios) { 
+
+        }
+        Student.printEightSemesterPlan();
+
+*/
+
+
+
+        //Student.printAllStudentPortfolios();
+        String filePath = "student_portfolio7.txt";
+        StudentPortfolio.printAllStudentPortfoliosToFile(filePath);
 
     }
 }
